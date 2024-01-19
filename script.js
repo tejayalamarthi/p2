ea68b25e79d3b27ba38ede58e1b6a43687f93c02
tejayalamarthi <75997087+tejayalamarthi@users.noreply.github.com>
2024-01-19 13:46:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var tablinks = document.getElementsByClassName("tab_links");
+var tabcontents = document.getElementsByClassName("tab_contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
